550044f6844dbccabb598d0bad8e3945b7f85fd4
Dhanuka99 <danuka1999lakshan@gmail.com>
2021-05-17 12:47:21 +0530

    How to use the Servlet Filters

--- 11-Filters/src/lk/ijse/filter/AFilter.java
+++ 11-Filters/src/lk/ijse/filter/AFilter.java
@@ -3,6 +3,8 @@ package lk.ijse.filter;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 
@@ -16,8 +18,18 @@ public class AFilter implements Filter {
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         System.out.println("D filter call");
+        servletRequest.setAttribute("x","Dhanuka");
         //methona thama ona dewal okkoma kliyannea..ex.selected users can be access the customer servlet
+
         filterChain.doFilter(servletRequest,servletResponse);
+
+        //response
+        servletResponse.setContentType("application/json");
+
+        //redirect the responce
+//        HttpServletResponse http = (HttpServletResponse) servletResponse;
+//        http.sendRedirect();
+
     }
 
     @Override
--- 11-Filters/src/lk/ijse/servlet/CustomerServlet.java
+++ 11-Filters/src/lk/ijse/servlet/CustomerServlet.java
@@ -13,5 +13,7 @@ public class CustomerServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 resp.getWriter().write("customer do get invoked");
+        String x = (String) req.getAttribute("x");
+        System.out.println(x);
     }
 }
